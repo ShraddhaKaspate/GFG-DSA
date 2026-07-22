5884308e306352daa6de0ed021b7bbbb218f7260
Shraddha Kaspate <shraddhakaspate80@gmail.com>
2026-07-22 22:54:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Consecutive Bit/max-consecutive-bit.java
+++ Difficulty: Easy/Max Consecutive Bit/max-consecutive-bit.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxConsecBits(int[] arr) {
         // code here
-        int max=1;
-        int cnt=1;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]==arr[i-1]){
-                cnt++;
+        int max=0,cnt=1;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                cnt+=1;
             }else{
+                max=Math.max(max,cnt);
                 cnt=1;
             }
-            max=Math.max(max,cnt);
         }
+        
+        max=Math.max(cnt,max);
         return max;
     }
 }
